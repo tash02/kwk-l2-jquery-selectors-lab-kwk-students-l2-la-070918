75291ac4c6d5498d70b9fac7da2850c7414f1f89
tash02 <tash.brinkley@gmail.com>
2018-07-13 21:32:04 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,2 +1,21 @@
 'use-strict';
 //add your code here
+function h1Selector (){
+ return $("h1")
+}
+
+function liInOlSelector (){
+  return $("ol li")
+}
+
+function linkSelector (){
+  return $("div#box4.box5 a")
+}
+
+function imageSelector (){
+  return $("img[alt='cat sleeping']")
+}
+
+function checkboxInputSelector (){
+  return $('[type="checkbox"]')
+}
